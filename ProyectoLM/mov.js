c7fc2201ddadad10731351f270d46224eb3d290e
Yavierr <91602515+Yavierr@users.noreply.github.com>
2021-12-11 15:19:09 +0100

    Add files via upload

---
+++ ProyectoLM/mov.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+     document.getElementById("sideNavigation").style.width="20%";
+    document.getElementById("main").style.marginleft="0px";              
+}
+function closeNav() {
+    document.getElementById("sideNavigation").style.width="0%";
+    document.getElementById("main").style.marginleft="0px";
+ }
+       
